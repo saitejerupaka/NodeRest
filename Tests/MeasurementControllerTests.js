77adecd3b9cfd27f88df89a4132d949b0d06986a
Saitej Erupaka <serupaka@gmail.com>
2015-12-11 21:19:18 -0600

    Setup Karma Mocha Chai

---
+++ Tests/MeasurementControllerTests.js
@@ -0,0 +1,11 @@
+describe('Measurement Controller Tests', function(){
+	
+	it('Canary Test', function(){
+			expect(true).to.equal(true);
+	});
+
+	describe('Post Measurement', function(){
+		var Measurement = [];
+
+	})
+});
